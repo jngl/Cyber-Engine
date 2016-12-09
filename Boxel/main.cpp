ef3a56e334acafcd23bdcb252bc38fbd6fe84d91
Jean-Noël Glesser <jngl92@gmail.com>
2016-12-09 21:21:00 +0100

    fixe sdl_main

--- Boxel/main.cpp
+++ Boxel/main.cpp
@@ -3,9 +3,15 @@
 
 #include <iostream>
 
+#include <SDL.h>
+
 #include "Boxel.hpp"
 
+#if defined(SDL_MAIN_NEEDED) || defined(SDL_MAIN_AVAILABLE)
 int SDL_main(int argc, char *argv[]){
+#else
+int main(){
+#endif
      try{
         boxel();
         return 0;
